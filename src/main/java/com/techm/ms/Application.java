4d5af0de0943872f71fc6ffd5f8d7eb903042a2e
swetha <swethakumar04@gmail.com>
2018-04-28 17:21:04 -0500

    Service implementation

--- src/main/java/com/techm/ms/Application.java
+++ src/main/java/com/techm/ms/Application.java
@@ -1,11 +1,13 @@
 package com.techm.ms;
 
+import io.swagger.annotations.SwaggerDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com")
